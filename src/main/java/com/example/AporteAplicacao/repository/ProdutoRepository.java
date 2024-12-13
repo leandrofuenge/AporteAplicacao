434beb428b364e813d9095640229b508690f5c31
Leandro Costa Magalhães Silva <leandrodevbackend@gmail.com>
2024-12-12 23:11:19 -0400

    update

--- src/main/java/com/example/AporteAplicacao/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AporteAplicacao.repository;
-
-import com.example.AporteAplicacao.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
